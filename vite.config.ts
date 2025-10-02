0ac6b2aad344570c9c2856939425c80e425005d4
AlejandroBast <nicohlas.personal@gmail.cm>
2025-10-02 00:32:55 -0500

    correcion de pagina

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Reproductor-SpotiFeev2/',   // 👈 cambia "TU-REPO" por el nombre exacto de tu repo en GitHub
-})
+  base: '/Reproductor-SpotiFeev2/',})
